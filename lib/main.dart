83698a024a7df051d7deedc561f57df3455d3fa4
Hicham Mallah <mallah.hicham@gmail.com>
2021-05-12 12:41:49 +0300

    removing deprecated flatbutton

--- lib/main.dart
+++ lib/main.dart
@@ -98,9 +98,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.green,
+                ),
+              ),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -117,8 +120,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.red,
+                ),
+              ),
               child: Text(
                 'False',
                 style: TextStyle(
